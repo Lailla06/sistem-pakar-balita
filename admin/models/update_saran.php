83521fca83ee2dbf6cb7c2129ae994233707e5b5
Lailla06 <laillatri@gmail.com>
2019-08-22 21:57:28 +0700

    update_saran

--- admin/models/update_saran.php
+++ admin/models/update_saran.php
@@ -2,7 +2,7 @@
 ob_start();
 require_once('../config/+koneksi.php');
 
-$conn   = mysqli_connect("localhost","root","","sistempakar");
+$conn = new Database("localhost","id10524213_lailla","pakarbalita123","id10524213_sistempakar");
 $query= mysqli_query($conn, "SELECT * FROM saran_user") or die("Failed to query database".mysql_error());;
 
 $nama = $_POST['nama'];
@@ -10,8 +10,8 @@ $email = $_POST['email'];
 $no_handphone = $_POST['no_handphone'];
 $pesan = $_POST['pesan'];
 
-mysql_connect("localhost", "root", "");
-mysql_select_db("sistempakar");
+mysql_connect("localhost","id10524213_lailla","pakarbalita123");
+mysql_select_db("id10524213_sistempakar");
 $query="INSERT INTO saran_user VALUES('','$nama','$email','$no_handphone','$pesan')";
 
 $hasil = mysql_query($query);
